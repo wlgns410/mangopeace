f218e4d55b367ddf571cf86fd2a433da3473611a
wonseok2877 <wonseok2877@gmail.com>
2021-07-07 13:23:12 +0900

    UPDATE : 수정사항 반영 models.py

--- restaurants/models.py
+++ restaurants/models.py
@@ -4,52 +4,50 @@ from django.db.models.fields         import CharField, DateTimeField, DecimalFie
 from django.db.models.fields.json    import JSONField
 from django.db.models.fields.related import ForeignKey
 
+from mangoPeace.common               import TimeStampModel
+
 class Menu(Model):
-    name                = CharField(max_length=45, unique=True)
+    name = CharField(max_length=45, unique=True)
 
     class Meta():
         db_table = "menus"
 
 class Category(Model):
-    menu                = ForeignKey(Menu, on_delete=PROTECT, related_name="category")
-    name                = CharField(max_length=45, unique=True)
+    menu = ForeignKey(Menu, on_delete=PROTECT, related_name="categories")
+    name = CharField(max_length=45, unique=True)
 
     class Meta():
         db_table = "categories"
 
 class SubCategory(Model):
-    category            = ForeignKey(Category, on_delete=PROTECT, related_name="sub_category")
-    name                = CharField(max_length=45, unique=True)
+    category = ForeignKey(Category, on_delete=PROTECT, related_name="sub_categories")
+    name     = CharField(max_length=45, unique=True)
 
     class Meta():
         db_table = "sub_categories"
 
-class Restaurant(Model):
-    sub_category        = ForeignKey(SubCategory, on_delete=PROTECT, related_name="restaurant")
-    name                = CharField(max_length=45)
-    address             = CharField(max_length=200, unique=True)
-    phone_number        = CharField(max_length=20, unique=True)
-    coordinate          = JSONField()
-    created_at          = DateTimeField(auto_now_add=True)
-    updated_at          = DateTimeField(auto_now=True)
-    open_time           = CharField(max_length=100)
+class Restaurant(TimeStampModel):
+    sub_category = ForeignKey(SubCategory, on_delete=PROTECT, related_name="restaurants")
+    name         = CharField(max_length=45)
+    address      = CharField(max_length=200, unique=True)
+    phone_number = CharField(max_length=20, unique=True)
+    coordinate   = JSONField()
+    open_time    = CharField(max_length=100)
 
     class Meta():
         db_table = "restaurants"
 
-class Food(Model):
-    restaurant          = ForeignKey(Restaurant, on_delete=CASCADE)
-    name                = CharField(max_length=45)
-    price_won           = DecimalField(max_digits=10, decimal_places=2)
-    created_at          = DateTimeField(auto_now_add=True)
-    updated_at          = DateTimeField(auto_now=True)
+class Food(TimeStampModel):
+    restaurant = ForeignKey(Restaurant, on_delete=CASCADE)
+    name       = CharField(max_length=45)
+    price      = DecimalField(max_digits=10, decimal_places=2)
 
     class Meta():
         db_table = "foods"
 
 class Image(Model):
-    food                = ForeignKey(Food, on_delete=CASCADE)
-    image_url           = URLField()
+    food      = ForeignKey(Food, on_delete=CASCADE)
+    image_url = URLField()
 
     class Meta():
         db_table = "food_images"

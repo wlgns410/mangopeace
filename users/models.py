9a2c44c3df6f608d1e3fa889aa816ee40ffd005f
wonseok2877 <wonseok2877@gmail.com>
2021-07-09 19:19:54 +0900

    MODIFY : users.models.py

--- users/models.py
+++ users/models.py
@@ -1,13 +1,19 @@
+import re
+
 from django.db.models.deletion       import CASCADE
 from django.db.models.fields         import CharField, IntegerField, TextField, URLField
 from django.db.models.fields.related import ForeignKey, ManyToManyField
 
 from restaurants.models              import Restaurant
 from mangoPeace.common               import TimeStampModel
-from users.validation                import validate_email, validate_nickname, validate_password, validate_phone_number
+
+NICKNAME_REGEX     = r'^[a-zA-Z가-힇0-9]{1,8}$'
+EMAIL_REGEX        = r'^[a-zA-Z0-9]+@[a-zA-Z0-9.]+\.[a-zA-Z0-9]+$'
+PASSWORD_REGEX     = r'^(?=.+[a-z])(?=.+[A-Z])(?=.+\d)(?=.+[!@#$%^&*()-=_+])[a-zA-Z0-9`~!@#$%^&*()_+-=,./<>?]{6,25}$'
+PHONE_NUMBER_REGEX = r'^01[1|2|5|7|8|9|0][0-9]{3,4}[0-9]{4}$'
 
 class User(TimeStampModel):
-    nickname            = CharField(max_length=15, unique=True)
+    nickname             = CharField(max_length=15, unique=True)
     email                = CharField(max_length=200, unique=True)
     password             = CharField(max_length=200)
     phone_number         = CharField(max_length=20, unique=True, null=True)
@@ -17,16 +23,16 @@ class User(TimeStampModel):
 
     @classmethod
     def validate(cls, data):
-        if not validate_email(data["email"]):
+        if not re.match(EMAIL_REGEX, data["email"]):
             return False
         
-        if not validate_nickname(data["nickname"]):
+        if not re.match(NICKNAME_REGEX, data["nickname"]):
             return False
         
-        if not validate_password(data["password"]):
+        if not re.match(PASSWORD_REGEX, data["password"]):
             return False
         
-        if not validate_phone_number(data["phone_number"]):
+        if not re.match(PHONE_NUMBER_REGEX, data["phone_number"]):
             return False
         
         return True

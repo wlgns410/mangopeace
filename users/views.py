840fa6792f955d69c13bb0c0f7ef0bdcbd742daa
wonseok2877 <wonseok2877@gmail.com>
2021-07-10 21:33:28 +0900

    ADD : 회원가입 기능

--- users/views.py
+++ users/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+import json
+import bcrypt
 
-# Create your views here.
+from django.views           import View
+from django.http            import JsonResponse
+from django.db.utils        import DataError, IntegrityError
+
+from json.decoder           import JSONDecodeError
+
+from users.models           import User
+
+class SignupView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            if not User.validate(data):
+                return JsonResponse({"message":"VALIDATION_ERROR"}, status=401)        
+
+            nickname        = data["nickname"]
+            email           = data["email"]
+            password        = data["password"]
+            phone_number    = data["phone_number"]
+            hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+            User.objects.create(
+            nickname     = nickname,
+            email        = email,
+            password     = hashed_password.decode(),
+            phone_number = phone_number,
+            )
+
+            return JsonResponse({"message":"success"}, status=201)
+
+        except JSONDecodeError:
+            return JsonResponse({"message":"JSON_DECODE_ERROR"}, status=400)        
+        
+        except KeyError:
+            return JsonResponse({"message":"KEY_ERROR"}, status=400)
+
+        except IntegrityError:
+            return JsonResponse({"message":"INTEGRITY_ERROR"}, status=400)
+
+        except DataError:
+            return JsonResponse({"message":"DATA_ERROR"}, status=400)

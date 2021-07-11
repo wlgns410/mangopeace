00df8c5e3618cf8b74c81e39b9d175ecbfff42a9
wonseok2877 <wonseok2877@gmail.com>
2021-07-12 00:06:14 +0900

    ADD : restaurant detail food view 기능 구현

--- restaurants/views.py
+++ restaurants/views.py
@@ -7,17 +7,31 @@ class RestaurantFoodView(View):
         try:
             foods_queryset = Restaurant.objects.get(id=restaurant_id).foods.all()
             foods          = []
-            
+            prices         = []
+
             for food_instance in foods_queryset:
-                
+                prices.append(food_instance.price)
+                images_queryset = Image.objects.filter(food=food_instance)
+                images          = []
+
+                for image_instance in images_queryset:
+                    images.append(image_instance.image_url)
+  
                 food = {
                     "id":food_instance.id,
                     "name":food_instance.name,
                     "price":food_instance.price,
+                    "images":images,
                 }
                 foods.append(food)
+
+            average_price = sum(prices) / len(prices)
+            result        = {
+                "foods":foods,
+                "average_price" : average_price
+            }
             
-            return JsonResponse({"message":"success", "result":foods}, status=200)
+            return JsonResponse({"message":"success", "result":result}, status=200)
 
         except Restaurant.DoesNotExist:
             return JsonResponse({"message":"RESTAURANT_NOT_EXISTS"}, status=400)

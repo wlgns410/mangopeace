0aa8d4b4cf836196a0a48044d540a35fe8a66c8f
wonseok2877 <wonseok2877@gmail.com>
2021-07-12 00:02:28 +0900

    ADD : restaurant detail view 기능구현

--- restaurants/views.py
+++ restaurants/views.py
@@ -10,7 +10,10 @@ class RestaurantDetailView(View):
             restaurant_instance = Restaurant.objects.get(id=restaurant_id)
             fake_user_instance  = User.objects.get(id=1)
             is_wished           = fake_user_instance.wishlist_restaurants.filter(id=restaurant_id).exists()
-            restaurant          = {
+            reviews_queryset    = Restaurant.objects.get(id=restaurant_id).review_set.all()
+            average_rating      = reviews_queryset.aggregate(Avg("rating"))["rating__avg"]
+            review_count        = Restaurant.objects.get(id=restaurant_id).review_set.all().count()
+            result              = {
             "id":restaurant_instance.id,
             "sub_category": restaurant_instance.sub_category.name,
             "name": restaurant_instance.name,
@@ -19,10 +22,12 @@ class RestaurantDetailView(View):
             "coordinate": restaurant_instance.coordinate,
             "open_time": restaurant_instance.open_time,
             "updated_at": restaurant_instance.updated_at,
-            "is_wished" : is_wished
+            "is_wished" : is_wished,
+            "review_count" : review_count,
+            "average_rating" : average_rating,
         }
 
-            return JsonResponse({"message":"success", "result":restaurant}, status=200)
-            
+            return JsonResponse({"message":"success", "result":result}, status=200)
+
         except Restaurant.DoesNotExist:
-            return JsonResponse({"message":"RESTAURANT_NOT_EXIST"}, status=400)        
+            return JsonResponse({"message":"RESTAURANT_NOT_EXIST"}, status=400)

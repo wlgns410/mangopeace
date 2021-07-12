7e50d78060c4caf6a098476c4b83e17ffe172f49
wonseok2877 <wonseok2877@gmail.com>
2021-07-12 21:17:46 +0900

    MODIFY : 리뷰 사항 반영

--- restaurants/views.py
+++ restaurants/views.py
@@ -14,25 +14,25 @@ class RestaurantDetailView(View):
             reviews                   = restaurant.review_set.all()
             average_rating            = reviews.aggregate(Avg("rating"))["rating__avg"]
             review_count              = {
-                "total" : reviews.count(),
-                "rating_one" : reviews.filter(rating=1).count(),
-                "rating_two" : reviews.filter(rating=2).count(),
+                "total"        : reviews.count(),
+                "rating_one"   : reviews.filter(rating=1).count(),
+                "rating_two"   : reviews.filter(rating=2).count(),
                 "rating_three" : reviews.filter(rating=3).count(),
-                "rating_four" : reviews.filter(rating=4).count(),
-                "rating_five" : reviews.filter(rating=5).count(),
+                "rating_four"  : reviews.filter(rating=4).count(),
+                "rating_five"  : reviews.filter(rating=5).count(),
             }
 
             result = {
-            "id":restaurant.id,
-            "sub_category": restaurant.sub_category.name,
-            "name": restaurant.name,
-            "address": restaurant.address,
-            "phone_number": restaurant.phone_number,
-            "coordinate": restaurant.coordinate,
-            "open_time": restaurant.open_time,
-            "updated_at": restaurant.updated_at,
-            "is_wished" : is_wished,
-            "review_count" : review_count,
+            "id"             :restaurant.id,
+            "sub_category"   : restaurant.sub_category.name,
+            "name"           : restaurant.name,
+            "address"        : restaurant.address,
+            "phone_number"   : restaurant.phone_number,
+            "coordinate"     : restaurant.coordinate,
+            "open_time"      : restaurant.open_time,
+            "updated_at"     : restaurant.updated_at,
+            "is_wished"      : is_wished,
+            "review_count"   : review_count,
             "average_rating" : average_rating,
             }
 

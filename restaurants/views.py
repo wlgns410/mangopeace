3f8b6e81614d5f67768ecb97565c642feeb59b09
wonseok2877 <wonseok2877@gmail.com>
2021-07-12 22:34:51 +0900

    MODIFY : 이전 PR 리뷰 토대로 수정

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,36 +1,36 @@
 from django.http        import JsonResponse
 from django.views       import View
+
 from restaurants.models import Restaurant
 
 class RestaurantReviewView(View):
     def get(self, request, restaurant_id):
         try:
-            UNIT_PER_PAGE    = 10
-            limit            = int(request.GET.get("limit", 1)) * UNIT_PER_PAGE
-            rating_min       = request.GET.get("rating-min", 0)
-            rating_max       = request.GET.get("rating-max", 5)
+            UNIT_PER_PAGE = 10
+            limit         = int(request.GET.get("limit", 1)) * UNIT_PER_PAGE
+            rating_min    = request.GET.get("rating-min", 0)
+            rating_max    = request.GET.get("rating-max", 5)
             
-            restaurant_instance = Restaurant.objects.get(id=restaurant_id)
-            reviews_queryset    = restaurant_instance.review_set
-            filtered            = reviews_queryset.filter(rating__gte = rating_min, rating__lte = rating_max).order_by("-created_at")[limit - UNIT_PER_PAGE : limit]
-            reviews             = []
+            restaurant  = Restaurant.objects.get(id=restaurant_id)
+            reviews     = restaurant.review_set.filter(rating__gte = rating_min, rating__lte = rating_max).order_by("-created_at")[limit - UNIT_PER_PAGE : limit]
+            review_list = []
 
-            for review_instance in filtered:
+            for r in reviews:
                 review = {
                     "user":{
-                        "id":review_instance.user.id,
-                        "nickname":review_instance.user.nickname,
-                        "profile_image":review_instance.user.profile_url if hasattr(review_instance.user, "profile_url") else None,
-                        "review_count":review_instance.user.reviewed_restaurants.count()
+                        "id":r.user.id,
+                        "nickname":r.user.nickname,
+                        "profile_image":r.user.profile_url if hasattr(r.user, "profile_url") else None,
+                        "review_count":r.user.reviewed_restaurants.count()
                     },
-                    "id":review_instance.id,
-                    "content" : review_instance.content,
-                    "rating":review_instance.rating,
-                    "created_at":review_instance.created_at,
+                    "id":r.id,
+                    "content" : r.content,
+                    "rating":r.rating,
+                    "created_at":r.created_at,
                 }
-                reviews.append(review)
+                review_list.append(review)
 
-            return JsonResponse({"message":"success", "result":reviews}, status=200)
+            return JsonResponse({"message":"success", "result":review_list}, status=200)
 
         except Restaurant.DoesNotExist:
-            return JsonResponse({"message":"RESTAURANT_NOT_EXIST"}, status=400)       
+            return JsonResponse({"message":"RESTAURANT_NOT_EXIST"}, status=404)

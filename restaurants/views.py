c0819aa60efa3866e83bae73a3778d60c8a9f0a1
wonseok2877 <wonseok2877@gmail.com>
2021-07-12 21:13:52 +0900

    MODIFY : 리뷰 사항 반영

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,19 +1,28 @@
 from django.http        import JsonResponse
 from django.views       import View
+from django.db.models import Avg
 
-from users.models       import User
 from restaurants.models import Restaurant
 
 class RestaurantDetailView(View):
+    @ConfirmUser
     def get(self, request, restaurant_id):
         try:
-            restaurant     = Restaurant.objects.get(id=restaurant_id)
-            user           = request.user
-            is_wished      = user.wishlist_restaurants.filter(id=restaurant_id).exists() if request.user
-            reviews        = Restaurant.objects.get(id=restaurant_id).review_set.all()
-            average_rating = reviews.aggregate(Avg("rating"))["rating__avg"]
-            review_count   = Restaurant.objects.get(id=restaurant_id).review_set.all().count()
-            result         = {
+            restaurant = Restaurant.objects.get(id=restaurant_id)
+            is_wished  = request.user.wishlist_restaurants.filter(id=restaurant_id).exists() if request.user else False
+
+            reviews                   = restaurant.review_set.all()
+            average_rating            = reviews.aggregate(Avg("rating"))["rating__avg"]
+            review_count              = {
+                "total" : reviews.count(),
+                "rating_one" : reviews.filter(rating=1).count(),
+                "rating_two" : reviews.filter(rating=2).count(),
+                "rating_three" : reviews.filter(rating=3).count(),
+                "rating_four" : reviews.filter(rating=4).count(),
+                "rating_five" : reviews.filter(rating=5).count(),
+            }
+
+            result = {
             "id":restaurant.id,
             "sub_category": restaurant.sub_category.name,
             "name": restaurant.name,
@@ -25,9 +34,9 @@ class RestaurantDetailView(View):
             "is_wished" : is_wished,
             "review_count" : review_count,
             "average_rating" : average_rating,
-        }
+            }
 
             return JsonResponse({"message":"success", "result":result}, status=200)
 
         except Restaurant.DoesNotExist:
-            return JsonResponse({"message":"RESTAURANT_NOT_EXIST"}, status=400)
+            return JsonResponse({"message":"RESTAURANT_NOT_EXIST"}, status=404)        

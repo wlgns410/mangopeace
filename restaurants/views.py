6b98e731579715debc8f484263538206d2ebe3d2
wonseok2877 <wonseok2877@gmail.com>
2021-07-12 21:19:57 +0900

    MODIFY : 리뷰 사항 반영

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,6 +1,6 @@
 from django.http        import JsonResponse
 from django.views       import View
-from django.db.models import Avg
+from django.db.models   import Avg
 
 from restaurants.models import Restaurant
 

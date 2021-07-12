889f3f010c6485334eec3429c0ab9e5073af2321
wonseok2877 <wonseok2877@gmail.com>
2021-07-12 22:19:38 +0900

    MODIFY : 이전 PR 리뷰 토대로 수정

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,29 +1,37 @@
-from django.http        import JsonResponse
-from django.views       import View
-from restaurants.models import Restaurant
+from django.http                 import JsonResponse
+from django.views                import View
+from django.db.models.aggregates import Avg
+
+from restaurants.models          import Image, Restaurant
 
 class RestaurantFoodView(View):
     def get(self, request, restaurant_id):
         try:
-            foods_queryset = Restaurant.objects.get(id=restaurant_id).foods.all()
-            foods          = []
-            prices         = []
+            foods         = Restaurant.objects.get(id=restaurant_id).foods.all()
+            average_price = foods.aggregate(Avg("price"))["price__avg"]
+            foods_list    = []
 
-            for food_instance in foods_queryset:
+            for f in foods:
+                images = Image.objects.filter(food=f)
+                images_list = []
+
+                for image in images:
+                    images_list.append(image.image_url)
+
                 food = {
-                    "id":food_instance.id,
-                    "name":food_instance.name,
+                    "id":f.id,
+                    "name":f.name,
+                    "price":f.price,
+                    "images":images_list,
                 }
-                foods.append(food)
-                prices.append(food_instance.price)
+                foods_list.append(food)
 
-            average_price = sum(prices) / len(prices)
-            result        = {
-                "foods":foods,
+            result = {
+                "foods"         : foods_list,
                 "average_price" : average_price
             }
             
             return JsonResponse({"message":"success", "result":result}, status=200)
 
         except Restaurant.DoesNotExist:
-            return JsonResponse({"message":"RESTAURANT_NOT_EXISTS"}, status=400)
+            return JsonResponse({"message":"RESTAURANT_NOT_EXISTS"}, status=404)

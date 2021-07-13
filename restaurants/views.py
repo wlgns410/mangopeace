589e0f86f996445059e0907557d30aa4d7e7d9cd
wonseok2877 <wonseok2877@gmail.com>
2021-07-13 17:31:32 +0900

    MODIFY : 리뷰 사항 반영

--- restaurants/views.py
+++ restaurants/views.py
@@ -2,13 +2,13 @@ from django.http                 import JsonResponse
 from django.views                import View
 from django.db.models.aggregates import Avg
 
-from restaurants.models          import Food, Restaurant
+from restaurants.models          import Food, Image, Restaurant
 
 class RestaurantFoodsView(View):
     def get(self, request, restaurant_id):
         try:
             foods      = Food.objects.filter(restaurant_id=restaurant_id)
-            foods_list = [{"id":f.id, "name":f.name, "price":f.price} for f in foods]
+            foods_list = [{"id":f.id, "name":f.name, "price":f.price, "images":[i.image_url for i in f.images.all()]} for f in foods]
             result     = {"foods" : foods_list}
             
             return JsonResponse({"message":"success", "result":result}, status=200)

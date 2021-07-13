e85d2246d9e70f3eba24a00ec21e9f49ccaa6405
wonseok2877 <wonseok2877@gmail.com>
2021-07-13 22:53:50 +0900

    MODIFY : resolved conflicts

--- restaurants/urls.py
+++ restaurants/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from restaurants.views import RestaurantFoodsView
+from restaurants.views import RestaurantDetailView, RestaurantFoodsView
 
 urlpatterns = [
     path("/<int:restaurant_id>/foods", RestaurantFoodsView.as_view()),
+    path("/<int:restaurant_id>", RestaurantDetailView.as_view()),  
+
 ]

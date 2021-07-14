a60e7e07eac979592a83d9c11f08bafd4616ac08
wonseok2877 <wonseok2877@gmail.com>
2021-07-14 22:55:59 +0900

    ADD : token decorator

--- users/utils.py
+++ users/utils.py
@@ -35,3 +35,31 @@ class ConfirmUser:
         
         except User.DoesNotExist:
             return JsonResponse({"message":"USER_NOT_EXIST"}, status=404)
+
+class LooseConfirmUser:
+    def __init__(self, func):
+        self.func = func
+    
+    def __call__(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get("Authorization")
+            
+            if access_token:
+                payload      = jwt.decode(jwt=access_token, key=my_settings.SECRET_KEY,  algorithms=my_settings.ALGORITHM)
+                request.user = User.objects.get(id=payload["id"])
+            else:
+                request.user = None
+            
+            return self.func(self, request, *args, **kwargs)
+        
+        except InvalidSignatureError:
+            return JsonResponse({"message":"INVALID_SIGNATURE"}, status=400)
+        
+        except ExpiredSignatureError:
+            return JsonResponse({"message":"EXPIRED_SIGNATURE"}, status=400)
+        
+        except DecodeError:
+            return JsonResponse({"message":"DECODE_ERROR"}, status=400)
+        
+        except User.DoesNotExist:
+            return JsonResponse({"message":"USER_NOT_EXIST"}, status=404)

ae8b3513bade46506d438fc8258efab732621096
wlgns410 <wlgns410@gmail.com>
2021-09-01 11:42:27 +0900

    ADD query_debugger & Modify urls.py

--- mangoPeace/urls.py
+++ mangoPeace/urls.py
@@ -29,7 +29,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users', include('users.urls')),
-    path("restaurants", include("restaurants.urls")),
+    path("", include("restaurants.urls")),
 ]
 
 if settings.DEBUG:
